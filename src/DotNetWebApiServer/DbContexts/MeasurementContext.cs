0be1f913233116d82cfccb74ad730adaccb1f250
Marcin Friedrich <fredros1996@gmail.com>
2019-03-27 17:58:01 +0100

    Added measurement db context

---
+++ src/DotNetWebApiServer/DbContexts/MeasurementContext.cs
@@ -0,0 +1,18 @@
+﻿using DotNetCoreWebApi.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreWebApi.DbContexts
+{
+    public class MeasurementContext : DbContext
+    {
+        public MeasurementContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Measurement> Measurements { get; set; }
+    }
+}
