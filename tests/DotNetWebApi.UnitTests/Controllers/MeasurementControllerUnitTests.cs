c0c9799c74f65bb69332ceef5731adedec1d7a47
Marcin Friedrich <fredros1996@gmail.com>
2019-04-10 18:25:44 +0200

    added basic measurement controller unit tests

---
+++ tests/DotNetWebApi.UnitTests/Controllers/MeasurementControllerUnitTests.cs
@@ -0,0 +1,47 @@
+﻿using DotNetCoreWebApi.Controllers;
+using DotNetCoreWebApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotNetWebApi.UnitTests.Controllers
+{
+    public class MeasurementControllerUnitTests
+    {
+        [Fact]
+        public async Task get_all_measurements()
+        {
+            // Arrange
+            var repository = MeasurementContextMocker.GetInMemoryMeasurementsRepository(nameof(get_all_measurements));
+            var controller = new MeasurementController(repository);
+
+            // Act
+            var response = await controller.GetAll() as ObjectResult;
+            var measurement = response.Value as List<Measurement>;
+
+            // Assert 
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal(5, measurement.Count);
+        }
+
+        [Fact]
+        public async Task get_all_measurements_with_existing_id()
+        {
+            // Arrange
+            var repository = MeasurementContextMocker.GetInMemoryMeasurementsRepository(nameof(get_all_measurements_with_existing_id));
+            var controller = new MeasurementController(repository);
+            var expectedValue = 0.25m;
+
+            // Act
+            var response = await controller.Get(1) as ObjectResult;
+            var measurement = response.Value as Measurement;
+
+            // Assert 
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal(expectedValue, measurement.Value);
+        }
+    }
+}
