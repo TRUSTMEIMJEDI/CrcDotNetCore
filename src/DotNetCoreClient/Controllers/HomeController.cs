3e2d1e6094f77b5c0337fad66850bfb26b02efa1
Marcin Friedrich <fredros1996@gmail.com>
2019-04-17 17:07:53 +0200

    Revert "Added blank measurements site with controller in HomeController"
    This reverts commit a99fcccd46ab009b097ef5a61b26a7726c1ef7cd.

--- src/DotNetCoreClient/Controllers/HomeController.cs
+++ src/DotNetCoreClient/Controllers/HomeController.cs
@@ -20,11 +20,6 @@ namespace DotNetCoreClient.Controllers
             return View();
         }
 
-        public IActionResult Measurements()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
