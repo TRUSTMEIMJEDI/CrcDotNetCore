093d8cc52b8235de4cf627ad865cfb67ae1b582a
Marcin Friedrich <fredros1996@gmail.com>
2019-04-24 18:06:43 +0200

    Added data-action type to button in form | Added implementation of removeBtn

--- src/DotNetCoreClient/wwwroot/js/measurementListSection.js
+++ src/DotNetCoreClient/wwwroot/js/measurementListSection.js
@@ -5,6 +5,12 @@
 
         let nameColumn = newRow.querySelector('div[data-column-type=\'name\']')
         let valueColumn = newRow.querySelector('div[data-column-type=\'value\']')
+        let removeBtn = newRow.querySelector('a[data-action=\'remove\']')
+
+        removeBtn.addEventListener('click', e => {
+            //removeBtn.parentElement.parentElement.remove()
+            newRow.remove()
+        })
 
         nameColumn.innerHTML = measurement.name
         valueColumn.innerHTML = measurement.value
