ed0ef2bc9038951efc663bbc1afba3b452d72ba4
Marcin Friedrich <fredros1996@gmail.com>
2019-04-24 17:47:32 +0200

    Added measurement list section

---
+++ src/DotNetCoreClient/wwwroot/js/measurementListSection.js
@@ -0,0 +1,15 @@
+﻿class MeasurementListSection {
+    addNewMeasurement(measurement) {
+        let grid = document.querySelector('#measurement_container')
+        let newRow = grid.children[1].cloneNode(true)
+
+        let nameColumn = newRow.querySelector('div[data-column-type=\'name\']')
+        let valueColumn = newRow.querySelector('div[data-column-type=\'value\']')
+
+        nameColumn.innerHTML = measurement.name
+        valueColumn.innerHTML = measurement.value
+
+        newRow.classList.remove('d-none')
+        grid.appendChild(newRow)
+    }
+}
