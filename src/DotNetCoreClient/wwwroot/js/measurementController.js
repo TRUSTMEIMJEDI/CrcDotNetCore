787c1f714b7763b4f26bcbe50057369566fcf0c6
Marcin Friedrich <fredros1996@gmail.com>
2019-04-24 17:49:19 +0200

    Added to EventListener that new measurement is added to list section

--- src/DotNetCoreClient/wwwroot/js/measurementController.js
+++ src/DotNetCoreClient/wwwroot/js/measurementController.js
@@ -1,9 +1,13 @@
 ﻿class MeasurementController {
     constructor() {
         this._newMeasurementSection = new NewMeasurementSection()
+        this._measurementListSection = new MeasurementListSection()
+
+        let _this = this
+
         this._newMeasurementSection.addEventListener(new class {
             newMeasurementAdded(e) {
-                debugger
+                _this._measurementListSection.addNewMeasurement(e)
             }
         })
     }
