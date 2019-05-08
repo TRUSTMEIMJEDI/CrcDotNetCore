5b170fb97dc33266d5f2e7b6b65c1512e0e0edab
Marcin Friedrich <fredros1996@gmail.com>
2019-05-08 18:19:21 +0200

    added Id and CreatedBy to MeasurementList

--- src/DotNetCoreClient/wwwroot/js/measurementListSection.js
+++ src/DotNetCoreClient/wwwroot/js/measurementListSection.js
@@ -6,8 +6,10 @@
         let grid = document.querySelector('#measurement_container')
         let newRow = grid.children[1].cloneNode(true)
 
+        let idColumn = newRow.querySelector('div[data-column-type=\'id\']')
         let nameColumn = newRow.querySelector('div[data-column-type=\'name\']')
         let valueColumn = newRow.querySelector('div[data-column-type=\'value\']')
+        let createdByColumn = newRow.querySelector('div[data-column-type=\'createdBy\']')
 
         //newRow.setAttribute('data-row-type', 'data')
         //newRow.setAttribute('data-internal-id', this._counter++)
@@ -18,8 +20,10 @@
         //newRow.dataset['rowType'] = 'data'
         //newRow.dataset['internalId'] = this._counter++
 
+        idColumn.innerText = measurement.id
         nameColumn.innerText = measurement.name
         valueColumn.innerText = measurement.value
+        createdByColumn.innerText = measurement.createdBy
 
         let removeAnchor = newRow.querySelector('a[data-action=\'remove\']')
         removeAnchor.addEventListener('click', e => {
