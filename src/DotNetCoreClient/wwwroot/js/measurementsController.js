b916dbf5bb1c87194eb1a08bf8673eb6245854fe
Marcin Friedrich <fredros1996@gmail.com>
2019-05-08 18:20:17 +0200

    Added add and get data from webapi to List

--- src/DotNetCoreClient/wwwroot/js/measurementsController.js
+++ src/DotNetCoreClient/wwwroot/js/measurementsController.js
@@ -1,7 +1,9 @@
 ﻿class MeasurementsController {
+    _serivce = null
     _editedRowId = -1
 
     constructor() {
+        this._serivce = new MeasurementService('http://localhost:54530/')
         this._newMeasurementSection = new NewMeasurementSection()
         this._measurementListSection = new MeasurementListSection()
 
@@ -9,7 +11,12 @@
 
         this._newMeasurementSection.addEventListener(new class {
             newMeasurementAdded(e) {
-                _this._newMeasurementSection_newMeasurementAdded(e)
+                let data = e
+                data.name = e.name
+                data.value = e.value
+                data.createdBy = 'Operator'
+                data.createdAt = '2019-04-19T09:11:40.019'
+                _this._serivce.post(data)
             }
 
             measurementModified(e) {
@@ -33,11 +40,31 @@
 
             }
         })
+
+        this._serivce.addEventListener(new class {
+            getResponseReady(e) {
+                JSON.parse(e.data).forEach(i => {
+                    _this._measurementListSection.addNewMeasurement({
+                        id: i.id,
+                        name: i.name,
+                        value: i.value,
+                        createdBy: i.createdBy
+                    })
+                })
+            }
+
+            postResponseReady(e) {
+                _this._measurementListSection.addNewMeasurement(JSON.parse(e.data))
+            }
+        })
+
+        this._serivce.get()
     }
 
-    _newMeasurementSection_newMeasurementAdded(e) {
-        this._measurementListSection.addNewMeasurement(e)
-    }
+
+    //_newMeasurementSection_newMeasurementAdded(e) {
+    //    _this._measurementListSection.addNewMeasurement(e)
+    //}
 }
 
 (() => new MeasurementsController())()
